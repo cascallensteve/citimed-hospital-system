2acac0babe1d94fc0acf6de717e82ff658cbaf4c
CascallenSteve <cascallensteve@gmail.com>
2025-10-03 16:31:02 +0300

    without old db

--- src/pages/admin/Patients.tsx
+++ src/pages/admin/Patients.tsx
@@ -9,6 +9,7 @@ import {
   CalendarDaysIcon,
   PencilIcon,
   TrashIcon,
+  EyeIcon,
 } from '@heroicons/react/24/outline';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
